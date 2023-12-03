d0ac7c1493a6237e341ba30193e7bb1c63d30a14
Kendry Grullon <kengrullon@gmail.com>
2023-12-03 11:44:48 -0400

    Hard one but very nice! #Day3

---
+++ src/03-gear-ratios/part1.ts
@@ -0,0 +1,56 @@
+import { readFileSync } from "node:fs";
+
+type Match = {
+  val: string;
+  idx: number;
+};
+
+const buffer = readFileSync("src/03-gear-ratios/input.txt");
+const input = buffer.toString().split("\n");
+
+const digitReg = /\d+/g;
+const symbolReg = /[^0-9a-zA-Z.]/g;
+
+let sum = 0;
+
+for (let idx = 0; idx < input.length; idx++) {
+  const nums = input[idx].matchAll(digitReg);
+  const matches: Match[] = Array.from(nums).map((match) => {
+    return { val: match[0], idx: match.index as number };
+  });
+  if (matches.length === 0) {
+    continue;
+  }
+  for (let i = 0; i < matches.length; i++) {
+    if (lookAround(matches[i], idx, input)) {
+      sum += parseInt(matches[i].val);
+    }
+  }
+}
+
+function lookAround(match: Match, idx: number, input: string[]): boolean {
+  const i = match.idx;
+  const endi = i + match.val.length;
+
+  for (
+    let iy = Math.max(idx - 1, 0);
+    iy <= Math.min(idx + 1, input.length - 1);
+    iy++
+  ) {
+    for (
+      let ix = Math.max(i - 1, 0);
+      ix <= Math.min(endi, input[idx].length - 1);
+      ix++
+    ) {
+      if (input[iy][ix].match(symbolReg) !== null) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(sum);
+
+// All the wrong answers I got:
+// 554197
---
+++ src/03-gear-ratios/part2.ts
@@ -0,0 +1,55 @@
+import { readFileSync } from "node:fs";
+
+type Match = {
+  val: string;
+  idx: number;
+};
+
+const buffer = readFileSync("src/03-gear-ratios/input.txt");
+const input = buffer.toString().split("\n");
+
+const digitReg = /\d+/g;
+const symbolReg = /[*]/g;
+
+let sum = 0;
+
+for (let idx = 0; idx < input.length; idx++) {
+  const nums = input[idx].matchAll(symbolReg);
+  const matches: Match[] = Array.from(nums).map((match) => {
+    return { val: match[0], idx: match.index as number };
+  });
+  if (matches.length === 0) {
+    continue;
+  }
+  for (let i = 0; i < matches.length; i++) {
+    sum += getTwoAround(matches[i], idx, input);
+  }
+}
+
+function getTwoAround(match: Match, idx: number, input: string[]) {
+  let nums: number[] = [];
+  for (
+    let yy = Math.max(idx - 1, 0);
+    yy <= Math.min(idx + 1, input.length - 1);
+    yy++
+  ) {
+    const line = Array.from(input[yy].matchAll(digitReg));
+    for (let i = 0; i < line.length; i++) {
+      const idx = line[i].index as number;
+      const val = line[i][0];
+      if (
+        (idx >= match.idx - 1 && idx <= match.idx + 1) ||
+        (idx + val.length - 1 >= match.idx - 1 &&
+          idx + val.length - 1 <= match.idx + 1)
+      ) {
+        nums.push(parseInt(val));
+      }
+    }
+  }
+  return nums.length === 2 ? nums[0] * nums[1] : 0;
+}
+
+console.log(sum);
+
+// All the wrong answers I got:
+// 90172708
