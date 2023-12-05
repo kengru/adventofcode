129d9d58b809ac40b74aefb1e9a31a04c5ac9b1f
Kendry Grullon <kengrullon@gmail.com>
2023-12-05 13:52:31 -0400

    The most inneficient thing I've created in my life.

---
+++ src/05-fertilizer/part1.ts
@@ -0,0 +1,31 @@
+import { readFileSync } from "node:fs";
+
+const buffer = readFileSync("src/05-fertilizer/input.txt");
+const [seeds, ...maps] = buffer.toString().split("\n\n");
+const seedList = seeds.split(": ")[1];
+
+const hugeMap = maps.map((map) => map.split("\n").slice(1));
+
+function getSeedTransform(seed: number, map: string[]) {
+  let location = seed;
+  return map.reduce((prev, current) => {
+    const [destination, source, range] = current.split(" ");
+    const sourceLocation = +source;
+    const destinationLocation = +destination;
+    const rangeSize = +range;
+    if (location >= sourceLocation && location < sourceLocation + rangeSize) {
+      return destinationLocation + (location - sourceLocation);
+    }
+    return prev;
+  }, seed);
+}
+
+const seedLocations = seedList.split(" ").map((seed) => {
+  let location = +seed;
+  hugeMap.forEach((map) => {
+    location = getSeedTransform(location, map);
+  });
+  return location;
+});
+
+console.log(Math.min(...seedLocations)); // 165788812
---
+++ src/05-fertilizer/part2.ts
@@ -0,0 +1,42 @@
+import { readFileSync } from "node:fs";
+
+const buffer = readFileSync("src/05-fertilizer/input.txt");
+const [seeds, ...maps] = buffer.toString().split("\n\n");
+const seedList = seeds.split(": ")[1];
+
+const hugeMap = maps.map((map) => map.split("\n").slice(1));
+
+function getSeedTransform(seed: number, map: string[]) {
+  let location = seed;
+  return map.reduce((prev, current) => {
+    const [destination, source, range] = current.split(" ");
+    const sourceLocation = +source;
+    const destinationLocation = +destination;
+    const rangeSize = +range;
+    if (location >= sourceLocation && location < sourceLocation + rangeSize) {
+      return destinationLocation + (location - sourceLocation);
+    }
+    return prev;
+  }, seed);
+}
+
+function getAllValues(seedList: string) {
+  const finalValues = [0];
+  let lowest = Infinity;
+  const values = seedList.split(" ");
+  for (let i = 0; i < values.length; i += 2) {
+    const start = +values[i];
+    const count = +values[i + 1];
+    for (let j = 0; j < count; j++) {
+      let location = start + j;
+      hugeMap.forEach((map) => {
+        location = getSeedTransform(location, map);
+      });
+      lowest = Math.min(lowest, location);
+    }
+  }
+  console.log(lowest);
+  return finalValues;
+}
+
+console.log(getAllValues(seedList));
