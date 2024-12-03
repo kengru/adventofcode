5f99fd55f9519e74002698b1c28888d4a1d71754
Kendry Grullon <kengrullon@gmail.com>
2024-12-03 13:24:11 -0400

    Fixing readability on 3-1

--- 2024/03-mull-it-over/part1.ts
+++ 2024/03-mull-it-over/part1.ts
@@ -2,19 +2,18 @@ import { readFileSync } from "node:fs";
 
 console.time("bench");
 
+function calc(tc: string): number {
+  tc = tc.replace("mul(", "");
+  tc = tc.replace(")", "");
+  const [n1, n2] = tc.split(",");
+  return +n1 * +n2;
+}
+
 const buffer = readFileSync("2024/03-mull-it-over/input.txt");
 const parsed = buffer.toString();
-let total = 0;
 
 const inst = parsed.match(/mul\(\d{1,3},\d{1,3}\)/g);
-if (inst) {
-  inst.forEach((v) => {
-    v = v.replace("mul(", "");
-    v = v.replace(")", "");
-    const [n1, n2] = v.split(",");
-    total += +n1 * +n2;
-  });
-}
+const total = inst?.reduce((prev, curr) => prev + calc(curr), 0);
 
 console.timeEnd("bench");
 
